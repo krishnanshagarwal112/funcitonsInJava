1dd4fb4c817bed39f2b16a7986a358a990da3185
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-06-04 00:32:52 +0530

    duh

--- src/StrExample.java
+++ src/StrExample.java
@@ -3,10 +3,18 @@ public class StrExample {
 
         String greeting = greet();
         System.out.println(greeting);
+
+        String personalised = myGreet("Kichi");
+        System.out.println(personalised);
+
+    }
+    static String myGreet(String name){
+        String message = "Hello " + name;
+        return message;
     }
     static String greet(){
 
-        String greeting = "Hello Krishnansh";
+        String greeting = "Hello folks";
         return greeting;
     }
 }
