cf72ed8e5d86a3e411caf3da5bf3fc6553aaefaf
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-06-04 00:25:50 +0530

    learning functions

---
+++ src/Methods.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Methods {
+    public static void main(String[] args) {
+        System.out.println(sum(5,19));      // Passing arguments in main funciton
+        }
+    static int sum(int a, int b){       // Creating arguments in sum function
+        int sum = a + b;
+        return sum;
+    }
+}
+
+
+
+// use static void if you want to return something
+// use static int if you want to return an integer
---
+++ src/StrExample.java
@@ -0,0 +1,12 @@
+public class StrExample {
+    public static void main(String[] args) {
+
+        String greeting = greet();
+        System.out.println(greeting);
+    }
+    static String greet(){
+
+        String greeting = "Hello Krishnansh";
+        return greeting;
+    }
+}
